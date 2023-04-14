3a579c177670e824c2cf6dc632cae3039387242e
Eggstory <khm9963@gmail.com>
2023-04-14 10:10:37 +0900

    commit

--- src/main/java/com/study/springboot/Ex19LoginJoinApplication.java
+++ src/main/java/com/study/springboot/Ex19LoginJoinApplication.java
@@ -2,11 +2,14 @@ package com.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class Ex19LoginJoinApplication {
 
 	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(Ex19LoginJoinApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
 		SpringApplication.run(Ex19LoginJoinApplication.class, args);
 	}
 
